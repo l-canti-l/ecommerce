f92d32635c53e02ab73088728bcda7204d2d2bb4
canti <ljdemar91@gmail.com>
2020-09-08 21:32:51 -0500

    finished navbar feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 import { 
     setHeaderLinks,
-    setNavbarLinks
+    setNavbarLinks,
+    changeNavbarActive
 } from './headernavbar';
 
 export {
     setHeaderLinks,
-    setNavbarLinks
+    setNavbarLinks,
+    changeNavbarActive
 };
+
