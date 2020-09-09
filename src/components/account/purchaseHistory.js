71455da44cb76374300d9f812e8005e44b516b24
canti <ljdemar91@gmail.com>
2020-09-09 13:52:52 -0500

    finished purchases component

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import PageTitle from "../pagetitle";
+
+import PageTitle from '../pagetitle';
 import Purchases from './purchases';
-import PurchaseDetail from "./purchaseDetail"
+import PurchaseDetail from './purchaseDetail';
 
 class PurchaseHistory extends Component {
-    render () {
+    render() {
         return (
-            <div className="purchase-history">
-                <PageTitle className="purchase-history__page-title" title="Purchase History" />
-                <Purchases className="purchase-history__purchases" />
-                <PurchaseDetail classname="purchase-history__detail" />
-                <div className="purchase-history__border-bottom"></div>
+            <div className='purchase-history'>
+                <PageTitle className='purchase-history__page-title' title='Purchase History'/>
+                <div className='purchase-history__content'>
+                    <Purchases className='purchase-history__purchases'/>
+                    <PurchaseDetail className='purchase-history__detail'/>
+                </div>
+                <div className='purchase-history__border-bottom'></div>
             </div>
         )
     }
