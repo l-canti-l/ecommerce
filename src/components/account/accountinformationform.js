6fc99ff9f15d8fb438a56434b40aeceb118b6ec4
canti <ljdemar91@gmail.com>
2020-09-13 22:50:34 -0500

    built long gray button component and placed instance of it on account information form to create change password button

--- src/components/account/accountinformationform.js
+++ src/components/account/accountinformationform.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { reduxForm, Field } from "redux-form";
 
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton, LongGrayButton } from "../formFields";
 
 import history from "../../history";
 
@@ -69,14 +69,15 @@ class AccountInformationForm extends Component {
      
 
         {/* <div className="account-information-form__line"></div> */}
-        {/* <Field
-          className="account-information-form__login"
-          onClick={() => history.push("/account")}
-          type="submit"
-          title="Login"
-          name="login"
-          component={FormButton}
-        /> */}
+        <Field
+          className="account-information-form__change-password"
+          onClick={() => console.log('trying to show passwords')}
+          type="button"
+          labelTitle="Password"
+          title="Change Password"
+          name="change-password"
+          component={LongGrayButton}
+        />
       </form>
     );
   }
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -22,8 +22,28 @@ export class FormButton extends Component {
     const { className, title, type, onClick, input, short } = this.props;
     return (
       <div className={`${className} form-button`}>
+        <label className="form-button__label">{title}</label>
         <button
           className={`form-button__button ${short ? 'form-button__gray-button' : ""}`}
+          type={type}
+          {...input}
+          onClick={onClick}
+        >
+          {title}
+        </button>
+      </div>
+    );
+  }
+}
+
+export class LongGrayButton extends Component {
+  render() {
+    const { className, title, type, onClick, input, labelTitle } = this.props;
+    return (
+      <div className={`${className} form-button-long-gray`}>
+        <label className="from-button-long-gray__label">{labelTitle}</label>
+        <button
+          className={`form-button-long-gray__button`}
           type={type}
           {...input}
           onClick={onClick}
