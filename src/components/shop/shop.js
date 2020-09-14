3929b8dea98014c6c1aceaaef15e38354c8af7c9
canti <ljdemar91@gmail.com>
2020-09-14 18:26:45 -0500

    developed shopReducer and fetch shop categories, action creator

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -13,6 +13,7 @@ class Shop extends Component {
             }
         ]
         this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories();
         //fetch shop products action creator
         //fetch navbar links 
             //set navbar links
