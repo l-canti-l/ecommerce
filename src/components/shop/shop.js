fd91a3768bcd8a33b6e605407348cee42f55d479
canti <ljdemar91@gmail.com>
2020-09-14 19:10:02 -0500

    refactored navbar to take additional onclick

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 class Shop extends Component {
@@ -14,24 +15,38 @@ class Shop extends Component {
         ]
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
+
+        // filter products with links
         this.props.fetchShopProducts();
     }
 
+    shouldComponentUpdate(nextProps) {
+        if(this.props != nextProps) {
+            this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
+        }
+        return true
+
+    }
+
     render() {
-        return(
-        <div className="shop">
-            {/* shop searchbar */}
-            {/* shop product component */}
-            {/* cart button */}
-        </div>
+
+        return (
+            <div className='shop'>
+                {/* shop search bar */}
+                {/* shop product */}
+                {/* shop cart button */}
+            </div>
         )
     }
 }
 
 function mapStateToProps(state) {
-    return { state }
+    const { categories } = state.shop;
+    return {
+        categories
+    } 
 }
 
-Shop = connect(mapStateToProps, actions)(Shop)
+Shop = connect(mapStateToProps, actions)(Shop);
 
 export default Shop;
