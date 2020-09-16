d00060338bcbf976f8f4d5fb7f639f3135f19f76
canti <ljdemar91@gmail.com>
2020-09-16 00:40:54 -0500

    structured shop cart component and styled

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,8 +4,17 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            showCart: true
+        }
+    }
 
     componentDidMount() {
         const headerLinks = [
@@ -34,6 +43,7 @@ class Shop extends Component {
     }
 
     render() {
+        return <ShopCart className='shop__cart'/>
 
         return (
             <div className='shop'>
@@ -42,11 +52,15 @@ class Shop extends Component {
                     {
                         this.props.filteredProducts.map(product => {
                             return (
-                               <ShopProduct {...product} key={product._id} />
+                                <ShopProduct {...product} key={product._id} />
                             )
                         })
                     }
                 </div>
+                {
+                    this.state.showCart ? <ShopCart className='shop__cart'/> : ''
+                }
+                
                 {/* shop cart button */}
             </div>
         )
