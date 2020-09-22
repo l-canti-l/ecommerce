63643d383b6616516bb492e9309898c0291720d5
canti <ljdemar91@gmail.com>
2020-09-22 18:21:19 -0500

    order review products title bar

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -1,39 +1,48 @@
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
-import history from '../../history';
+import history from "../../history";
 
-import { FormButton } from '../formFields';
-import ReviewProducts from './reviewProducts';
+import { FormButton } from "../formFields";
+import ReviewProducts from "./reviewProducts";
 
 class ReviewForm extends Component {
-    render() {
-        const { className, handleSubmit } = this.props;
+  render() {
+    const { className, handleSubmit } = this.props;
 
-        return (
-            <form onSubmit={handleSubmit} className={`${className} review-form`}>
-                <ReviewProducts className='review-form__products'/>
-                <div className='review-form__line'></div>
-                <Field className='review-form__proceed'
-                onClick={() => history.push('/signin')}
-                type='submit'
-                title='Proceed to Checkout'
-                name='proceed'
-                component={FormButton}/>
-                <Field className='review-form__back'
-                onClick={() => history.push('/shop')}
-                type='button'
-                title='Back'
-                name='back'
-                short={true}
-                component={FormButton}/>
-            </form>
-        )
-    }
+    return (
+      <form onSubmit={handleSubmit} className={`${className} review-form`}>
+        <div className="review-form__legend">
+          <div className="review-form__legend__name">Name</div>
+          <div className="review-form__legend__quantity">Quantity</div>
+          <div className="review-form__legend__price">Price</div>
+        </div>
+        <ReviewProducts className="review-form__products" />
+        <div className="review-form__line"></div>
+        <Field
+          className="review-form__proceed"
+          onClick={() => history.push("/signin")}
+          type="submit"
+          title="Proceed to Checkout"
+          name="proceed"
+          component={FormButton}
+        />
+        <Field
+          className="review-form__back"
+          onClick={() => history.push("/shop")}
+          type="button"
+          title="Back"
+          name="back"
+          short={true}
+          component={FormButton}
+        />
+      </form>
+    );
+  }
 }
 
 ReviewForm = reduxForm({
-    form: 'ReviewForm'
+  form: "ReviewForm",
 })(ReviewForm);
 
 export default ReviewForm;
